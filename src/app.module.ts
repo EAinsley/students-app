afc91a4d738cbafbe5357621b6ed814dc61c5a81
Elia El Atram <eliaatram@outlook.com>
2026-04-29 19:27:28 +0200

    feat: schema validation + config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentsModule } from './students/students.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(5432),
+        DATABASE_USER: Joi.required(),
+      }),
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'pass123',
-      database: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT!,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       autoLoadEntities: true, // models will be loaded automatically
       synchronize: false, // entities will be synced with db (not recommended in production)
       poolSize: 10,
--- src/students/students.module.ts
+++ src/students/students.module.ts
@@ -5,9 +5,10 @@ import { StudentsController } from './students.controller';
 import { StudentsService } from './students.service';
 import { Course } from './entites/course.entity';
 import { Event } from '../events/entities/event.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Student, Course, Event])],
+  imports: [TypeOrmModule.forFeature([Student, Course, Event]), ConfigModule],
   controllers: [StudentsController],
   providers: [StudentsService],
 })
