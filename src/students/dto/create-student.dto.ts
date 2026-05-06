49b0683b94cdc0822134c1bbb437fdd9ef448a8c
Elia El Atram <eliaatram@outlook.com>
2026-05-06 19:57:52 +0200

    feat: add OpenApi Swagger documentation

--- src/students/dto/create-student.dto.ts
+++ src/students/dto/create-student.dto.ts
@@ -1,13 +1,17 @@
 import { IsObject, IsString } from 'class-validator';
 import { Course } from '../entites/course.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateStudentDto {
+  @ApiProperty({ description: 'this is a name' })
   @IsString()
   readonly name: string;
 
+  @ApiProperty()
   @IsString()
   readonly promotion: string;
 
+  @ApiProperty()
   @IsObject({ each: true })
   readonly courses: Course[];
 }
